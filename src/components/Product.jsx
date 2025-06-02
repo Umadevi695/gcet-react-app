576b0d933c0a7ecd335771979fd4e1b072e84ac9
Umadevi695 <uma970179@gmail.com>
2025-06-02 15:22:47 +0530

    updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,27 @@
+import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "../App";
-
+import axios from "axios";
 export default function Product() {
   const { user } = useContext(AppContext);
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    const res = await axios.get("http://localhost:8080/products");
+    setProducts(res.data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
     <div>
-      <h3>Welcome {user.name}!</h3>
+      <h3>Welcome {user.name}! </h3>
       Product List
+      {products &&
+        products.map((value) => (
+          <li key={value.id}>
+            {value.name}-{value.price}
+          </li>
+        ))}
     </div>
   );
 }
