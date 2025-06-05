178e9b49f53572f16547d14967ef716275f171de
Umadevi695 <uma970179@gmail.com>
2025-06-05 17:19:29 +0530

    updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,8 +4,8 @@ import { AppContext } from "../App";
 import axios from "axios";
 import "./Product.css";
 export default function Product() {
-  const { user } = useContext(AppContext);
-  const [products, setProducts] = useState([]);
+  const { user, products, setProducts, cart, setCart } = useContext(AppContext);
+  // const [products, setProducts] = useState([]);
   const API = import.meta.env.VITE_API_URL;
   const fetchProducts = async () => {
     const res = await axios.get(`${API}/products/all`);
@@ -14,16 +14,21 @@ export default function Product() {
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  const addToCart = (id) => {
+    !cart[id] && setCart({ ...cart, [id]: 1 });
+  };
+
   return (
     <div>
-      <h3>Welcome.. {user.name}! </h3>
+      <h3>Welcome {user.name}! </h3>
       <div className="App-Product-Row">
         {products &&
           products.map((value) => (
             <div key={value._id}>
               <h3>{value.name}</h3>
               <h4>{value.price}</h4>
-              <button>Add to Cart</button>
+              <button onClick={() => addToCart(value.pid)}>Add to Cart</button>
             </div>
           ))}
       </div>
